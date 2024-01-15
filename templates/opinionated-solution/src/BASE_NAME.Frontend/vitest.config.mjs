4f5f086a44ef917f4e9a5f304bb89fa0f35f3cb7
Karl-Johan Sjögren <karl-johan.sjogren@xlent.se>
2024-01-15 09:21:44 +0100

    Disabled vitest globals

--- templates/opinionated-solution/src/BASE_NAME.Frontend/vitest.config.mjs
+++ templates/opinionated-solution/src/BASE_NAME.Frontend/vitest.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true,
     setupFiles: ['./scripts/test-setup.mjs'],
     reporters: ['verbose'],
     coverage: {
