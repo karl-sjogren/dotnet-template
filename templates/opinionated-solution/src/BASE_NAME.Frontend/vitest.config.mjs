e04b072677aeb09f0abea072d387232489f2ee4d
Karl-Johan Sjögren <karl-johan.sjogren@xlent.se>
2024-01-15 09:07:35 +0100

    Updated vitest stuff

--- templates/opinionated-solution/src/BASE_NAME.Frontend/vitest.config.mjs
+++ templates/opinionated-solution/src/BASE_NAME.Frontend/vitest.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
+    setupFiles: ['./scripts/test-setup.mjs'],
     reporters: ['verbose'],
     coverage: {
       reporter: ['text', 'cobertura', 'html']
