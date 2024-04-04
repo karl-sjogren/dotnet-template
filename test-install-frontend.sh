1b824b93c839176420f34a9e8590e104bc76b6c0
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2024-04-04 23:26:23 +0200

    Final stuff

--- test-install-frontend.sh
+++ test-install-frontend.sh
@@ -1,13 +1,14 @@
-rm -rf ../dotnet-template-test-output
+rm -rf ../dotnet-template-test-output-frontend
 dotnet new install ./templates/opinionated-solution --force
-dotnet new karls-solution -o ../dotnet-template-test-output -n TestSolution --addFrontendProject
+dotnet new karls-solution -o ../dotnet-template-test-output-frontend -n TestSolution --addFrontendProject
 dotnet new uninstall ./templates/opinionated-solution
 
-cd ../dotnet-template-test-output/
+cd ../dotnet-template-test-output-frontend/
 dotnet build
 dotnet test
 
 cd src/TestSolution.Frontend/
+rm -rf ./node_modules
 yarn
 yarn lint:css
 yarn lint:js
---
+++ test-install-github.sh
@@ -0,0 +1,10 @@
+rm -rf ../dotnet-template-test-output-github
+dotnet new install ./templates/opinionated-solution --force
+dotnet new karls-solution -o ../dotnet-template-test-output-github -n TestSolution --includeGithubActions
+dotnet new uninstall ./templates/opinionated-solution
+
+cd ../dotnet-template-test-output-github/
+dotnet build
+dotnet test
+
+cd /../dotnet-template/
