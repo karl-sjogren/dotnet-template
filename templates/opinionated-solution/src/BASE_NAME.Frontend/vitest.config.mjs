2a7925cdee1139df9fc403c388ff8b77a62087fe
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2025-01-16 14:52:39 +0100

    Updated vitest.config.mjs with new default values

--- templates/opinionated-solution/src/BASE_NAME.Frontend/vitest.config.mjs
+++ templates/opinionated-solution/src/BASE_NAME.Frontend/vitest.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     setupFiles: ['./scripts/test-setup.mjs'],
-    reporters: ['verbose'],
+    reporters: ['dot'],
+    environment: 'jsdom',
+    isolate: false,
     coverage: {
       reporter: ['text', 'cobertura', 'html']
     }
